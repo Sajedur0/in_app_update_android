869acdc57bb959b324b449fc6c82f4671c1f5650
Sajedur0 <31769074+Sajedur0@users.noreply.github.com>
2026-06-28 11:36:13 +0600

    Major Update!

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:in_app_update_android_example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('App renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(const InAppUpdateExampleApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('In-App Update Example'), findsOneWidget);
+    expect(find.text('Check for Update'), findsOneWidget);
+    expect(find.text('Perform Immediate Update'), findsOneWidget);
+    expect(find.text('Start Flexible Update'), findsOneWidget);
+    expect(find.text('Complete Flexible Update'), findsOneWidget);
   });
 }
